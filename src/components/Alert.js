3a3ebef01b65092c76e5fa39b504432f5d5e19f2
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-15 21:06:56 +0300

    add Alert

---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+function Alert(props) {
+    const {name} = props;
+
+    return <div id="toast-container">
+        <div className="toast">
+            {name} добавлен в корзину
+        </div>
+    </div>
+}
+export {Alert}
--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -14,7 +14,8 @@ function BasketList(props) {
     }, 0)
 
     return <ul className="collection basket-list">
-        <a href="#!" className="collection-item active">Корзина</a>
+        <li
+            className="collection-item active">Корзина</li>
         <i className="material-icons basket-close" onClick={handleBasketShow}>close</i>
         {
             order.length ? order.map(item => (
@@ -29,10 +30,10 @@ function BasketList(props) {
                 <li className="collection-item"> Корзина пуста</li>
         }
 
-        <a href="#!" className="collection-item active">
+        <li className="collection-item active">
             Общая стоимость: {totalPrice} руб.
         <button className="secondary-content btn">Оформить</button>
-        </a>
+        </li>
     </ul>
 }
 
