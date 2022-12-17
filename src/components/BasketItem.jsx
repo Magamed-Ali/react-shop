4f04382e429ee4587270a987a7dfd2f012a334d2
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-17 13:02:32 +0300

    fixed //

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -11,23 +11,19 @@ function BasketItem(props) {
 
     return (
         <li className="collection-item">
-
             <span className="collection-name">
                 {name}
                 *
                 {quantity} = {price * quantity} руб.
             </span>
-
             <span>
                 <i className='material-icons secondary-content'
                    onClick={() => decQuantity(id)}>remove_circle</i>
                 <i className='material-icons secondary-content'
                    onClick={() => incQuantity(id)}>add_circle</i>
-
                 <i className="material-icons secondary-content secondary-close"
                    onClick={() => removeFromBasket(id)} >close</i>
             </span>
-
         </li>
     )
 }
