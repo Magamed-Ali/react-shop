cc5e3f09c9483cb9f6fa7b6eef5809b978c6d01c
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2022-12-18 12:10:25 +0300

    add addToBasket/2

--- src/reducer.js
+++ src/reducer.js
@@ -39,6 +39,41 @@ export function reducer(state, {type, payload}) {
                 alertName: payload.name
             }
         }
+        case 'INCREMENT_QUANTITY':
+            return {
+                ...state,
+                order: state.order.map(el => {
+                    if (el.id === payload.id) {
+                        const newQuantity = el.quantity + 1;
+                        return {
+                            ...el,
+                            quantity: newQuantity
+                        }
+                    } else {
+                        return el;
+                    }
+                })
+            }
+        case 'DECREMENT_QUANTITY':
+            return {
+                ...state,
+                order: state.order.map(el => {
+                    if (el.id === payload.id) {
+                        const newQuantity = el.quantity - 1;
+                        return {
+                            ...el,
+                            quantity: newQuantity >= 0 ? newQuantity : 0
+                        }
+                    } else {
+                        return el;
+                    }
+                })
+            }
+        case 'TOGGLE_BASKET':
+            return {
+                ...state,
+                isBasketShow: !state.isBasketShow
+            }
         default:
             return state;
     }
